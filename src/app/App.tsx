de7820e4dd3ed2692343d24614d9441ac859c66e
sherlie <dodoinwl@gmail.com>
2026-03-23 22:23:46 +0200

    Fix errors; update nav styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "../pages/HomePage";
 import CategoriesPage from "../pages/CategoriesPage";
-import Navbar from "../entities/navbar/NavBar";
+import Navbar from "../entities/navbar/Navbar";
 
 function App() {
   return (
--- src/entities/navbar/Navbar.css.ts
+++ src/entities/navbar/Navbar.css.ts
@@ -1,29 +1,34 @@
-import { style } from '@vanilla-extract/css';
+import { style } from "@vanilla-extract/css";
 
 export const nav = style({
   position: "fixed",
   bottom: "1em",
   left: 0,
   width: "100%",
-  backgroundColor: 'rgb(119, 91, 145)',
+  backgroundColor: "rgb(119, 91, 145)",
 });
 
 export const navList = style({
-  listStyle: 'none',
-  display: 'flex',
-  gap: '1rem',
+  listStyle: "none",
+  display: "flex",
+  paddingInlineStart: 0,
+  marginBlockStart: 0,
+  marginBlockEnd: 0,
+  margin: 0,
 });
 
 export const navLink = style({
-  textDecoration: 'none',
-  fontWeight: 'bold',
-  color: 'white',
-  padding: 16,
-})
+  textDecoration: "none",
+  fontWeight: "bold",
+  color: "white",
+  display: "flex",
+  alignItems: "center",
+  padding: "0.7rem",
+});
 
 export const activeNavLink = style([
-    navLink, 
-    {
-        backgroundColor: 'rgb(199, 157, 239)',
-    }
+  navLink,
+  {
+    backgroundColor: "rgb(199, 157, 239)",
+  },
 ]);
